1252f7ef54a960bc79426322fe5342a3dfff3ae4
Gigeone <matthieugh@gmail.com>
2023-03-12 11:59:22 +0100

    deploy

---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+import { contractABI, contractAddress } from '../utils/constants'
+
+export const TransactionContext = React.createContext()
+
+const { ethereum } = window
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum)
+  const signer = provider.getSigner()
+  const transactionsContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  )
+  return transactionsContract
+}
+
+export const TransactionsProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [formData, setFormData] = useState({
+    addressTo: '',
+    amount: '',
+    keyword: '',
+    message: '',
+  })
+  const [transactions, setTransactions] = useState([])
+  const [isLoading, setIsLOading] = useState(false)
+  const [transactionsCount, setTransactionsCount] = useState(
+    localStorage.getItem('transactionsCount')
+  )
+
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }))
+  }
+
+  const getAllTransactions = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask')
+      const transactionsContract = getEthereumContract()
+      const availableTransactions =
+        await transactionsContract.getAllTransactions()
+
+      const structuredTransactions = availableTransactions.map(
+        (transaction) => ({
+          addressTo: transaction.receiver,
+          addressFrom: transaction.sender,
+          timestamp: new Date(
+            transaction.timestamp.toNumber() * 1000
+          ).toLocaleString(),
+          message: transaction.message,
+          keyword: transaction.keyword,
+          amount: parseInt(transaction.amount._hex) / 10 ** 18,
+        })
+      )
+      setTransactions(structuredTransactions)
+      console.log(availableTransactions)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask')
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+      console.log(accounts)
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0])
+        getAllTransactions()
+      } else {
+        console.log('No accounts found')
+      }
+    } catch (error) {
+      console.log(error)
+      throw new Error('No ethereum object')
+    }
+  }
+
+  const checkIfTransactionsExist = async () => {
+    try {
+      if (ethereum) {
+        const transactionsContract = getEthereumContract()
+        const transactionsCount =
+          await transactionsContract.getTransactionsCount()
+        window.localStorage.setItem('transactionsCount', transactionsCount)
+      }
+    } catch (error) {
+      console.log(error)
+
+      throw new Error('No ethereum object')
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask')
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+      throw new Error('No ethereum object')
+    }
+  }
+
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert('Please Install Metamask')
+      const { addressTo, amount, keyword, message } = formData
+      const transactionsContract = getEthereumContract()
+      const parsedAmount = ethers.utils.parseEther(amount)
+
+      await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: currentAccount,
+            to: addressTo,
+            gas: '0x5208', //21000 GWEI
+            value: parsedAmount._hex,
+          },
+        ],
+      })
+
+      const transactionHash = await transactionsContract.addToBlockchain(
+        addressTo,
+        parsedAmount,
+        message,
+        keyword
+      )
+
+      setIsLoading(true)
+      console.log(`Loading - ${transactionHash.hash}`)
+      await transactionHash.wait()
+      console.log(`Success - ${transactionHash.hash}`)
+      setIsLoading(false)
+
+      const transactionsCount = await transactionsContract.getTransactionsCount()
+
+      setTransactionsCount(transactionsCount.toNumber())
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+
+      throw new Error('No ethereum object')
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+    checkIfTransactionsExist()
+  }, [])
+
+  return (
+    <TransactionContext.Provider
+      value={{
+        transactionsCount,
+        connectWallet,
+        transactions,
+        currentAccount,
+        isLoading,
+        sendTransaction,
+        handleChange,
+        formData,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  )
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractAddress = '0x5421DC56dB9d5fACd75AD36508B3a45a2154Ea15'
+export const contractABI = abi.abi
